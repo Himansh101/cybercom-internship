48ad03320d09a6eb4b268bce16efe55ad4c0f7eb
Himanshu2912-debug <koshtihimanhu3@gmail.com>
2026-02-12 11:59:33 +0530

    add public access modifier before condstruct magic method

--- mvc/app/code/Core/Controllers/Front.php
+++ mvc/app/code/Core/Controllers/Front.php
@@ -2,7 +2,7 @@
 class Core_Controllers_Front{
     protected $_request;
 
-    function __construct()
+    public function __construct()
     {
         $admin = new Core_Controllers_Admin();
         echo "<pre>";
